ad9fece48e4a6413f7d929b2d9f12be6ff934cff
toymakercody <cody@archonweb.com>
2014-12-13 23:16:54 -0500

    Created the plugins meta data
    Checked on the test site and while it won't activate just yet. It has
    the starting of a plugin

---
+++ membership-management.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: Membership Management
+ * Plugin URI: http://archonweb.com/membership-management/
+ * Description: Allows the ability to setup and manage multiple membership tiers.
+ * Version: 1.0
+ * Author: Cody Greene
+ * Author URI: https://github.com/toymakercody
+ * License: GPL2
+ *
+ */
+?>
