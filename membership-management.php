79088c5c8f39c89074c55d951817d800fa9ab057
toymakercody <cody@archonweb.com>
2014-12-16 16:31:46 -0500

    Added activation and deactivation functions
    When activated the plugin adds a column to the wp_users table.
    When deactivated the plugin removes the column from the wp_users table.

--- membership-management.php
+++ membership-management.php
@@ -9,15 +9,41 @@
  * License: GPL2
  *
  */
+
 /*
  * Assign global variables
  */
 
 
-// Hook for adding admin menus
+
+/*
+ * Create the extra column on the user table needed for plugin when plugin is activated
+ */
+function mma_edit_user_table(){
+	global $wpdb;
+	$sql = "ALTER TABLE wp_users ADD membership_management_level int";
+	$wpdb->query($sql);
+}
+register_activation_hook(__FILE__, 'mma_edit_user_table');
+
+/*
+ * Remove the extra column on the user table needed for plugin when plugin is deactivated
+ */
+function mma_delete_column_user_table(){
+	global $wpdb;
+	$sql = "ALTER TABLE wp_users DROP COLUMN membership_management_level";
+	$wpdb->query($sql);
+}
+register_deactivation_hook(__FILE__, 'mma_delete_column_user_table');
+
+/*
+ * Hook for adding admin menus
+ */
 add_action('admin_menu', 'mt_add_pages');
 
-// action function for above hook
+/*
+ * action function for above hook
+ */
 function mt_add_pages() {
 	global $plugin_url;
 	// Add a new top-level menu (ill-advised):
@@ -33,7 +59,9 @@ function mt_add_pages() {
 
 }
 
-// membership_management_page() displays the page content for the custom Test Toplevel menu
+/*
+ * membership_management_page() displays the page content for the custom Test Toplevel menu
+ */
 function membership_management_page() {
     echo "<h2>" . "Membership Management" . "</h2>";
 }
