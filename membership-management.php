946c2a3024bdddacd6d4f74520e4fce252b071ff
toymakercody <cody@archonweb.com>
2014-12-15 23:21:00 -0500

    added to the admin side panel
    also added custom icon for the side panel

--- membership-management.php
+++ membership-management.php
@@ -9,4 +9,33 @@
  * License: GPL2
  *
  */
+/*
+ * Assign global variables
+ */
+
+
+// Hook for adding admin menus
+add_action('admin_menu', 'mt_add_pages');
+
+// action function for above hook
+function mt_add_pages() {
+	global $plugin_url;
+	// Add a new top-level menu (ill-advised):
+	add_menu_page(
+		__('Membership Management','Membership Management'),
+		__('Membership Management','Membership Management'),
+		'manage_options',
+		'manage_options',
+		'membership_management_page',
+		WP_PLUGIN_URL . '/membership-management/img/contact_card.png'
+		
+	);
+
+}
+
+// membership_management_page() displays the page content for the custom Test Toplevel menu
+function membership_management_page() {
+    echo "<h2>" . "Membership Management" . "</h2>";
+}
+
 ?>
